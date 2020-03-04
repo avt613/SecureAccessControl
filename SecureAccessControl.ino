62b2bf25912044abc8e61cee6dd1e6c1a83dd5c8
avt613 <47078235+avt613@users.noreply.github.com>
2020-03-04 11:20:27 +0000

    Made function to reset read UID variables

--- SecureAccessControl.ino
+++ SecureAccessControl.ino
@@ -3,50 +3,40 @@
 *   4 - failed to write to Log 
 */
 
-// use pixels.setPixelColor(LED#, Color); to set a LED color (Red/ Green/ Blue/ Off)
-// pixels.clear(); will set all LEDs to off
-// and then use pixels.show();
-
+// Setting up the NeoPixel Strip
 #include <Adafruit_NeoPixel.h>
 #define NeoPixelPin 2 // Which pin on the Arduino is connected to the NeoPixels?
 #define NumNeoPixels 30 // How many NeoPixels are attached to the Arduino?
+#define NeoPixelNotify 0 // Determine which LED to use as a notification LED
 Adafruit_NeoPixel pixels(NumNeoPixels, NeoPixelPin, NEO_GRB + NEO_KHZ800);
-static uint32_t Red   = pixels.Color(255,   0,   0);
-static uint32_t Green = pixels.Color(  0, 255,   0);
-static uint32_t Blue  = pixels.Color(  0,   0, 255);
-static uint32_t Off   = pixels.Color(  0,   0,   0);
-#define NeoPixelNotify 0
-
+static uint32_t Red   = pixels.Color(255,   0,   0);  // Setting easy name for common colours for the LED's 
+static uint32_t Green = pixels.Color(  0, 255,   0);  // use pixels.setPixelColor(LED#, Color); to set a LED color (Red/ Green/ Blue/ Off)
+static uint32_t Blue  = pixels.Color(  0,   0, 255);  // pixels.clear(); will set all LEDs to off
+static uint32_t Off   = pixels.Color(  0,   0,   0);  // and then use pixels.show();
+// Setting up the RFID and SD modules
 #include <MFRC522.h> // for the RFID
 #include <SPI.h> // for the RFID and SD card module
 #include <SD.h> // for the SD card
-
-// define pins for RFID
-#define CS_RFID 10
-#define RST_RFID 9
-// define select pin for SD card module
-#define CS_SD 3
-
-
+#define CS_RFID 10    // define pins for RFID
+#define RST_RFID 9    // define pins for RFID
+#define CS_SD 3   // define select pin for SD card module
+MFRC522 rfid(CS_RFID, RST_RFID); // Instance of the class for RFID
 File myFile; // Create a file to store the data
 String LogFile = "log.txt"; // name of Log File
-
-
-MFRC522 rfid(CS_RFID, RST_RFID); // Instance of the class for RFID
-
-
 String uidString;// Variable to hold the tag's UID
 
 //*****************************************************************************************//
 
 void setup() {
+  // Setup NeoPixels
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
   pixels.setBrightness(30); // Set BRIGHTNESS to about 1/5 (max = 255)
   pixels.clear(); // Set all pixel colors to 'off'
   pixels.show();
+  
   Serial.begin(9600); // Init Serial port
 //  while(!Serial); // Wait for computer serial box
-  SPI.begin(); // Init SPI bus
+  SPI.begin(); // Init SPI bus for RFID and SD modules
   
   // Setup RFID module
   rfid.PCD_Init(); // Init MFRC522
@@ -70,6 +60,7 @@ void loop() {
     pixels.show();
     readRFID();
     verifyRFIDCard();
+    ResetRFIDReadVariables();
   }
   delay(10);
 }
@@ -90,19 +81,9 @@ void readRFID() {
 void verifyRFIDCard(){
   if(SD.exists(uidString + ".txt")){
     AccessGranted(3000);
-    uidString = "0";
-    rfid.uid.uidByte[0] = 00;
-    rfid.uid.uidByte[1] = 00;
-    rfid.uid.uidByte[2] = 00;
-    rfid.uid.uidByte[3] = 00;
   }
   else{
     AccessDenied(1000);
-    uidString = "0";
-    rfid.uid.uidByte[0] = 00;
-    rfid.uid.uidByte[1] = 00;
-    rfid.uid.uidByte[2] = 00;
-    rfid.uid.uidByte[3] = 00;
   }
   Serial.println("");
 }
@@ -168,4 +149,14 @@ void FlashNeoPixel(int PixelNum, int NumberOfTimesToFlah, int FlashDelayTime, ui
     pixels.show();
     delay(FlashDelayTime);
   }
+}
+
+//*******************************************************************************************//
+
+void ResetRFIDReadVariables(){
+    uidString = "0";
+    rfid.uid.uidByte[0] = 00;
+    rfid.uid.uidByte[1] = 00;
+    rfid.uid.uidByte[2] = 00;
+    rfid.uid.uidByte[3] = 00;
 }
