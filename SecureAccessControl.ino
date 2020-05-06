f91d5cd2dda53255acb748698031b9a2f0a293b6
avt613 <47078235+avt613@users.noreply.github.com>
2020-05-06 11:38:25 +0100

    cleaned up

--- SecureAccessControl.ino
+++ SecureAccessControl.ino
@@ -7,45 +7,50 @@
 *   8 - Could not add new card
 */
 
-#define AccessGrantedTime 3000 // Time to keep the door unlocked for in milliseconds
-#define AccessDeniedTime  1000 // Time to wait after an incorrect unlock attempt in milliseconds
-// Setup RTC Module
-#include <Wire.h>
-#include <RTClib.h>
-RTC_DS1307 rtc; 
-// Setting up the NeoPixel Strip
-#include <Adafruit_NeoPixel.h>
+#include <Wire.h>                 // RTC Module
+#include <RTClib.h>               // RTC Module
+#include <Adafruit_NeoPixel.h>    // NeoPixel Strip
+#include <SPI.h>                  // for the RFID and SD card module
+#include <SD.h>                   // for the SD card
+#include <MFRC522.h>              // for the RFID
+
+#define AccessGrantedTime 3000    // Time to keep the door unlocked for in milliseconds
+#define AccessDeniedTime  1000    // Time to wait after an incorrect unlock attempt in milliseconds
+// PINS
 #define NeoPixelPin     2 // Which pin on the Arduino is connected to the NeoPixels?
+#define relay           3 // Set Relay Pin
+#define CS_SD           4 // define select pin for SD card module
+#define prog            6 // Button pin for Programming Mode
+#define CS_RFID         8 // define pins for RFID
+#define RST_RFID        9 // define pins for RFID
+
 #define NumNeoPixels    1 // How many NeoPixels are attached to the Arduino?
 #define NeoPixelNotify  0 // Determine which LED to use as a notification LED
+
+byte    sector  =       2;// which sector to read on the RFID card
+String  LogFile = "LOG.TXT";      // name of Log File
+String  ACBits  = " 0f 00 ff 69"; // AC bits the cards must have
+String  uidString;        // Variable to hold the tag's UID
+File    myFile;       // Create a file to store the data
+MFRC522::MIFARE_Key keyA = {keyByte: {0x35, 0x35, 0x35, 0x32, 0x39, 0x34}};
+MFRC522::MIFARE_Key keyB = {keyByte: {0x00, 0x00, 0xC8, 0xFF, 0x7F, 0x00}};
+
+RTC_DS1307 rtc; 
+MFRC522 rfid(CS_RFID, RST_RFID);
 Adafruit_NeoPixel pixels(NumNeoPixels, NeoPixelPin, NEO_GRB + NEO_KHZ800);
 static uint32_t Red   = pixels.Color(255,   0,   0);  // Setting easy name for common colours for the LED's 
 static uint32_t Green = pixels.Color(  0, 255,   0);  // use pixels.setPixelColor(LED#, Color); to set a LED color (Red/ Green/ Blue/ Off)
 static uint32_t Blue  = pixels.Color(  0,   0, 255);  // pixels.clear(); will set all LEDs to off
 static uint32_t Off   = pixels.Color(  0,   0,   0);  // and then use pixels.show();
-// Setting up the RFID and SD modules
-#include <SPI.h>      // for the RFID and SD card module
-#include <SD.h>       // for the SD card
-#include <MFRC522.h>  // for the RFID
-#define CS_RFID 8     // define pins for RFID
-#define RST_RFID 9    // define pins for RFID
-#define CS_SD 4       // define select pin for SD card module
-MFRC522 rfid(CS_RFID, RST_RFID);  // Instance of the class for RFID
-MFRC522::MIFARE_Key keyA = {keyByte: {0x35, 0x35, 0x35, 0x32, 0x39, 0x34}};
-MFRC522::MIFARE_Key keyB = {keyByte: {0x00, 0x00, 0xC8, 0xFF, 0x7F, 0x00}};
-File    myFile;       // Create a file to store the data
-String  LogFile = "LOG.TXT";      // name of Log File
-String  uidString;    // Variable to hold the tag's UID
-String  ACBits = " 0f 00 ff 69";  // AC bits the cards must have
-byte    sector = 2;   // which sector to read on the RFID card
-#define relay    3    // Set Relay Pin
-#define prog     6    // Button pin for Programming Mode
+
 
 //*****************************************************************************************//
 
 void setup() {  
+  randomSeed(analogRead(0));  // used to generate sudo random numbers
   pinMode(relay, OUTPUT);
   digitalWrite(relay, LOW);   // Make sure door is locked
+  pinMode(prog, INPUT_PULLUP);   // Enable pin's pull up resistor
   Serial.begin(9600);         // Init Serial port
   // while(!Serial);          // Wait for computer serial box
   // Setup NeoPixels
@@ -69,7 +74,6 @@ void setup() {
   }
   
   SPI.begin(); // Init SPI bus for RFID and SD modules
-  
   // Setup SD card module
   if(!SD.begin(CS_SD)) {
     Serial.println(F("Initializing SD card failed!"));
@@ -84,11 +88,6 @@ void setup() {
     }
   //rfid.PCD_SetAntennaGain(rfid.RxGain_max); // increases the range of the RFID module
   
-  FlashNeoPixel(NeoPixelNotify, 1, 250, Red);
-  FlashNeoPixel(NeoPixelNotify, 1, 250, Blue);
-  FlashNeoPixel(NeoPixelNotify, 1, 250, Green);
-
-  randomSeed(analogRead(0));  // used to generate sudo random numbers
   Serial.print(F("Using key for A: "));
     for (byte i = 0; i < MFRC522::MF_KEY_SIZE; i++) {
         Serial.print(keyA.keyByte[i] < 0x10 ? " 0" : " ");
@@ -104,19 +103,19 @@ void setup() {
   Serial.print(F("Using AC bits  : "));
   Serial.println(ACBits);
   
-  pinMode(prog, INPUT_PULLUP);   // Enable pin's pull up resistor
   
   LogToSD(F("Startup Initialising Complete"));
   Serial.println(F("Startup Initialising Complete"));
-  
+  FlashNeoPixel(NeoPixelNotify, 1, 250, Red);
+  FlashNeoPixel(NeoPixelNotify, 1, 250, Blue);
+  FlashNeoPixel(NeoPixelNotify, 1, 250, Green);
 }
 
 //*****************************************************************************************//
 
 void loop() {
-  digitalWrite(CS_RFID, LOW);
   //look for new cards
-  //Serial.println(rfid.PICC_IsNewCardPresent());
+  Serial.println(rfid.PICC_IsNewCardPresent());
   if(rfid.PICC_IsNewCardPresent()) {
     readRFID();
     if(uidString != "00000000"){  // ignore if it didn't read the card properly
@@ -369,28 +368,6 @@ void ResetRFIDReadVariables(){
     }
 }
 
-//******************************************************************************************//
-/* not needed
-byte DecToHex(byte Dec){
-  byte DecToHex[] = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
-                      0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
-                      0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F,
-                      0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F,
-                      0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F,
-                      0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F,
-                      0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F,
-                      0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E, 0x7F,
-                      0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F,
-                      0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F,
-                      0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF,
-                      0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF,
-                      0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
-                      0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF,
-                      0xE0, 0xE1, 0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xEB, 0xEC, 0xED, 0xEE, 0xEF,
-                      0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF}; 
-  return DecToHex[Dec]; 
-}
-*/
 //*******************************************************************************************//
 /*
 void ManagerMode(){
